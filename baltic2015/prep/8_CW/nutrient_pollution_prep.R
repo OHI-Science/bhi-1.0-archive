9155c325730684f733c6352bebab70650cbc9eda
lvikt <lena.viktorsson@gmail.com>
2015-08-13 17:08:54 +0200

    Cleaned prep code and made final tables for input to layers moved raw data folder to private repository BHI-issues

--- baltic2015/prep/8_CW/nutrient_pollution_prep.R
+++ baltic2015/prep/8_CW/nutrient_pollution_prep.R
@@ -3,7 +3,7 @@
 
 # SGNAme = taxon_name_key
  rm(list = ls())
-
+#
 # library(dplyr)    # install.packages('dplyr')   for data manipulation
 # library(tidyr)    # install.packages('tidyr')   for data manipulation
 # library(stringr)  # install.packages('stringr') for string manipulation
@@ -13,16 +13,19 @@
 # setwd('baltic2015/prep/8_CW')
 # list.files()
 #
-# # reading data
+# # reading data ####
 
-DIP <- read.table(file = 'DIP_JC.csv', header = TRUE, sep = ",")
-TARGETS <- read.table(file = "TARGETS_JC.csv", header = TRUE, sep = ",")
-rgn_id <- read.table(file = "rgn_id.csv", header = TRUE, sep = ",")
+dip <- read.table(file = '../../../../BHI-issues/raw_data/DIP_JC.csv', header = TRUE, sep = ",")
+nu_poll <- read.table(file = '../../../../BHI-issues/raw_data/nu_pollution.csv', header = TRUE, sep = ",")
+targets <- read.table(file = "../../../../BHI-issues/raw_data/TARGETS_JC.csv", header = TRUE, sep = ",")
+rgn_id <- read.table(file = "../../layers/rgn_global_gl2014.csv", header = TRUE, sep = ",", stringsAsFactors = F)
 
-# renaming basins to fit to rgn_id. NB! TEMPORARY SOLUTION UNTIL WE HAVE DEFINITE DECISION ON BASINS.
+# renaming basins to fit to rgn_id. ####
+ # NB! TEMPORARY SOLUTION UNTIL WE HAVE DEFINITE DECISION ON BASINS.
 
-DIP =
-  DIP %>%
+dip =
+  dip %>%
+#    as.factor(Name) %>%
   rename(KT = Kattegat,
          BP = Baltic_Proper,
          GF = Gulf_of_Finland,
@@ -33,25 +36,25 @@ DIP =
          AR = Arkona_Basin,
          BH = Bornholm_Basin,
          DS = Danish_Straits) %>%
-  gather(basin, value, -Name, -year)
+  gather(basin, value, -Name, -year) %>%
+   arrange(Name)
 
 # creating target value for Baltic Proper as a mean of EGB, WGB and NBP
 BP =
-TARGETS %>%
+targets %>%
    select(Eastern_Gotland_Basin, Western_Gotland_Basin, Northern_Baltic_Proper)
 BP = rowMeans(BP)
- TARGETS = mutate(TARGETS, BP = BP)
+ targets = mutate(targets, BP = BP)
 
-TARGETS =
-  TARGETS %>%
-#    rowMeans(select(Eastern_Gotland_Basin, Western_Gotland_Basin, Northern_Baltic_Proper))) %>%
+targets =
+  targets %>%
   rename(KT = Kattegat,
          GF = Gulf_of_Finland,
          GR = Gulf_of_Riga,
          AA = Aland_Sea,
          BS = Bothnian_Sea,
          BB = Bothnian_Bay,
-         AR = Arkona_Sea,        #from here down different names than in DIP-file
+         AR = Arkona_Sea,        #from here down different names than in nu_poll-file
          BH = Bornholm_Sea,
          SO = The_Sound,
          GB = Great_Belt,
@@ -64,58 +67,56 @@ TARGETS =
          QK = The_Quark) %>%
    gather(basin, value, -Name) %>%
    rename(target = value)
-#  TARGETS =
-#    rename(TARGETS, target = value)
-
-
-#
-#  TARGETS %>%
-#    group_by(Name) %>%
-#    filter(basin %in% c('EG','WG','NB')) %>%
-#    summarise(mean(value)) %>%
-#    mutate(basin = "BP") %>%
 
 ## Calculting status as measured value divided by target
-
 results =
-   full_join(filter(TARGETS, Name == 'Winter_DIP'), DIP, by = "basin")
-results =
-   results %>%
-#    rename(TARGETS = value.x, value = value.y) %>%
+   full_join(filter(targets, Name == "Winter_DIP"), dip, by = "basin") %>%
    mutate(status = value/target)
 
- # Add values for basins in the straits, the Sound and Western Baltic.
+ # Add values for basins in the straits, the Sound and Western Baltic. ####
  # Using timeseries for Danish straits and target values for the Sound and the Great Belt.
  # TODO: use MB for GE-WB and KB for DE-WB
- DS = filter(DIP, basin == "DS")
- SO_target = as.numeric(select(filter(TARGETS, basin == "SO" & Name == "Winter_DIP"),target))
- GB_target = as.numeric(select(filter(TARGETS, basin == "GB" & Name == "Winter_DIP"),target))
- WB_results =
-   DS %>%
-   mutate(status = value/GB_target, basin = "WB", target = GB_target, value = NA)
- SO_results =
-   DS %>%
-   mutate(status = value/SO_target, basin = "SO", target = SO_target, value = NA)
+ fix.basin <- function(value_basin, target_basin, new_basin, var_name, val_df, target_df) {
+   val_basin = filter(val_df, basin == value_basin)
+   xx_target = as.numeric(select(filter(target_df, basin == target_basin & Name == var_name), target))
+   xx_results =
+     val_basin %>%
+     mutate(status = value/xx_target, basin = new_basin, target = xx_target, value = NA)
+ }
+ SO_results = fix.basin("DS","SO","SO", "Winter_DIP",dip, targets)
+ WB_results = fix.basin("DS","GB","WB", "Winter_DIP",dip, targets)
 
  results = bind_rows(results, WB_results, SO_results)
 
- short_results =
-   results %>%
-   group_by(basin) %>%
-   filter(!is.na(status) & year == 2011)
-
- # calculate trend using lm(). Coefficient[[2]] give the slope and [[1]] the intercept
+ # calculate trend using lm(). Coefficient[[2]] give the slope and [[1]] the intercept ####
  future =
-   filter(results, !is.na(status)) %>%
-   group_by(basin) %>%
+   filter(results, !is.na(status)) %>% group_by(basin) %>%
    do(trend = lm(status ~ year, data = .)) %>%
-#    do(data.frame(var = names(coef(.$trend)),
-#                  coef = coef(.$trend),
-#                  basin = .$basin)) %>%
-   do(data.frame(slope = coef(.$trend)[["year"]],
+   do(tbl_df(data.frame(slope = coef(.$trend)[["year"]],
                  intercept = coef(.$trend)[["(Intercept)"]],
-                 future_status = coef(.$trend)[["year"]]*2016))
+                 future_status = (coef(.$trend)[["year"]]*2016 + coef(.$trend)[["(Intercept)"]]) /
+                   (coef(.$trend)[["year"]]*2011 + coef(.$trend)[["(Intercept)"]]),
+                 basin = .$basin)))
 
+ ## make final data tables for layers output ####
+
+ trend_score =
+   select(future, basin, future_status)
+ status_score =
+   results %>% filter(!is.na(status)) %>% group_by(basin) %>%
+   filter(status == last(status)) %>%
+   select(basin, status)
+
+ # join calculated scores to rgn_ids to get values for each region
+ tmp = mutate(rgn_id, basin = as.table(matrix(unlist(strsplit(rgn_id$label, "_")), ncol = 2, byrow= T))[,2])
+ status_score = full_join(tmp, status_score, by = "basin") %>%
+   select(rgn_id, label, status) %>%
+   filter(!is.na(label))
+ trend_score = full_join(tmp, trend_score, by = "basin") %>%
+   select(rgn_id, label, future_status) %>%
+   filter(!is.na(label))
+
+#### Plot to check data ####
 
 # library(ggplot2)
  filter(results, !is.na(status)) %>%
