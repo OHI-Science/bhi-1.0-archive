83c8efbe3e514a328ded82981e08f046be8b31da
lvikt <lena.viktorsson@gmail.com>
2015-11-25 09:41:40 +0100

    Commiting smaller changes to be able to merge branches.

--- baltic2015/prep/8_CW/secchi.R
+++ baltic2015/prep/8_CW/secchi.R
@@ -17,6 +17,9 @@ dbClearResult(t)
 # data<- secchi %>%
 #   mutate(date =  as.Date(yyyy.mm.ddThh.mm), month = as.numeric(format(date, "%m"))) %>%
 #   rename(basin = Assessment.Unit.METAVAR.TEXT)
+rgn_id <- read.table(file = "~/github/bhi/baltic2015/layers/rgn_global_gl2014.csv", header = TRUE, sep = ",", stringsAsFactors = F)
+rgn_id <- rename(rgn_id, BHI_ID = rgn_id)
+
 data <- data %>%
   mutate(country = paste(substring(Assessment_unit, 1, 3)))
 
@@ -24,15 +27,29 @@ summer_secchi <-
   data %>%
   filter(Month %in% c(6:9), HELCOM_COASTAL_CODE > 0) %>%
   mutate(country = paste(substring(Assessment_unit, 1, 3))) %>%
-  group_by(BHI_ID) %>%
-  summarise(secchi = mean(secchi, na.rm = T), year = mean(Year, na.rm = T), country =max(country)) %>%
-#   filter(grepl("DEN|SWE|FIN|GER|POL|RUS|LIT|LAT|EST", HELCOM_ID)) %>%
+  group_by(BHI_ID, Year) %>%
+  summarise(secchi = mean(secchi, na.rm = T), year = mean(Year, na.rm = T), country =max(country))
 
+summer_secchi_opensea <-
+  data %>%
+  filter(Month %in% c(6:9), HELCOM_COASTAL_CODE <= 0) %>%
+  group_by(BHI_ID, Year) %>%
+  summarise(secchi = mean(secchi, na.rm = T), year = mean(Year, na.rm = T), HELCOM_ID =max(HELCOM_ID))
 
+# write.csv(summer_secchi,'~/github/BHI-issues/raw_data/ICES/summer_secchi.csv')
+# write.csv(summer_secchi_opensea,'~/github/BHI-issues/raw_data/ICES/summer_secchi_opensea.csv')
 
 windows()
 ggplot(summer_secchi) +
   geom_point(aes(x = year, y = secchi, color = country)) +
-  geom_point(data = filter(data, HELCOM_COASTAL_CODE > 0), aes(x = Year, y = secchi, color = country)) +
+#   geom_point(data = filter(data, HELCOM_COASTAL_CODE > 0), aes(x = Year, y = secchi, color = country)) +
+  xlim(1995, 2015) +
+  facet_wrap(~BHI_ID) +
+  ggtitle("Coastal areas")
+
+windows()
+ggplot() +
+  geom_point(data = summer_secchi_opensea, aes(x = year, y = secchi), color = 'blue') +
+  geom_point(data = summer_secchi, aes(x = year, y = secchi), color = 'red') +
   xlim(1995, 2015) +
   facet_wrap(~BHI_ID)
