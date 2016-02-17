e6b83c37609c57e8d089c910c54349f748991263
jennifergriffiths <jennifer.griffiths@su.se>
2016-02-17 17:22:42 +0100

    Calculating percent coastal tourism from NUTS1

--- baltic2015/prep/7_TR/accommodation_stays_prep.R
+++ baltic2015/prep/7_TR/accommodation_stays_prep.R
@@ -1,5 +1,12 @@
 ##Data prep -- TR -- Accommodation Stays
 
+##Goal of data prep file
+#have data on Total nights in accommodations by NUTS2 regions
+#have percent coastal stays by NUTS1 regions
+#get this percentage (averaged from 2012-2014) and apply to the NUTS2 data #have not done
+# Divide this by the coastal area of the NUTS2 region (km2 with 1km buffer inland?), call this Nac  #have not done
+#create Accom value for BHI region by taking:  sum [ Nac(nuts associated) * area (nuts associated)] #have not done
+
 library(package = dplyr)
 library(package = tidyr)
 library(package = ggplot2)
@@ -74,24 +81,7 @@ colnames(bhi.factor)[1]="NUTS2_ID"
 bhi.accom.join = full_join(accom.data.bhi, bhi.factor, by="NUTS2_ID")
 glimpse(bhi.accom.join)
 
-##########
-###Clean accom.coast.data###
 
-#are there coast data that are allocated to NUTS2 for BHI_relevant regions
-accom.coast.bhi = accom.coast.data%>%filter(BHI_relevant=="NUTS2")%>% #only data associated with BHI_ID
-  select(-starts_with("BHI"))%>%  #remove the BHI_IDs, deal with separately
-  select(-TERRTYPO,-GEO_LABEL, -INDIC_TO,-INDIC_TO_LABEL,-UNIT,-UNIT_LABEL,-NACE_R2, -NACE_R2_LABEL,-TIME_LABEL)#reduce number of columns
-
-accom.coast.bhi
-#There is no NUTS2 data with Coastal info - must be at a higher spatial resolution
-
-
-#work with all GEO units that have coastal and total info - get a percentage coastal see how it looks
-accom.coast.data%>% select(-TERRTYPO,-GEO_LABEL, -INDIC_TO,-INDIC_TO_LABEL,-UNIT,-UNIT_LABEL,-NACE_R2, -NACE_R2_LABEL,-TIME_LABEL)%>%#reduce number of columns
-  select(-starts_with("BHI"))%>% #remove BHI_ID, just look at the coastal data
-
-
-#################################
 #Plotting accommodation stays from the accomm.data (this does not separate coastal v. non-coastal)
 #unique NUTS2_ID
 unique(accom.data.bhi$NUTS2_ID)
@@ -110,5 +100,69 @@ ggplot(bhi.accom.join )+geom_point(aes(YEAR,TotalNights, color=NUTS2_ID)) +
   facet_wrap(~BHI_ID,scales = "free")
 
 
+##########
+###Clean accom.coast.data###
 
+#are there coast data that are allocated to NUTS2 for BHI_relevant regions
+#accom.coast.bhi = accom.coast.data%>%filter(BHI_relevant=="NUTS2")%>% #only data associated with BHI_ID
+#  select(-starts_with("BHI"))%>%  #remove the BHI_IDs, deal with separately
+#  select(-TERRTYPO,-GEO_LABEL, -INDIC_TO,-INDIC_TO_LABEL,-UNIT,-UNIT_LABEL,-NACE_R2, -NACE_R2_LABEL,-TIME_LABEL)#reduce number of columns
+#accom.coast.bhi
+##There is no NUTS2 data with Coastal info - must be at a higher spatial resolution
+
+
+#Figure out what NUTS1 level each NUTS2 level is associated with - data only given at NUTS1 level
+NUTS.levels =  accom.coast.data%>%filter(BHI_relevant=="NUTS2")%>% #only data associated with BHI_ID
+  select(-starts_with("BHI"))%>%  #remove the BHI_IDs, deal with separately
+  select(-TERRTYPO,-GEO_LABEL, -INDIC_TO,-INDIC_TO_LABEL,-UNIT,
+         -UNIT_LABEL,-NACE_R2, -NACE_R2_LABEL,-TIME_LABEL) #reduce number of columns
+
+colnames(NUTS.levels)[2]="NUTS2_ID" #rename column 1
+
+
+NUTS.levels = NUTS.levels%>% mutate(NUTS1_ID = substr(NUTS2_ID,1,3))%>% #create NUTS1_ID, should be equal to the first 3 characters in NUTS2_ID
+  select(-TERRTYPO_LABEL,-TIME,-Value,-Flag.and.Footnotes)%>% #get rid of extra columns
+  distinct(NUTS1_ID,NUTS2_ID)
+head(NUTS.levels)
+
+
+#get coast-no coast accomm data associated with the NUTS1_IDs that are BHI relevant
+#will apply the % coastal from NUTS1 regions to the NUTS2 data
+
+
+accom.coast.data$Value = as.numeric(accom.coast.data$Value) #make Value numeric
+
+accom.coast.bhi = left_join(NUTS.levels,
+                            select(accom.coast.data,TERRTYPO,GEO,TIME,Value,Flag.and.Footnotes),
+                            by=c("NUTS1_ID"="GEO")) %>%  #select the data from NUTS1
+  group_by(NUTS2_ID,NUTS1_ID,TIME,Flag.and.Footnotes)%>%
+  spread(TERRTYPO,Value)%>%
+  summarise(PERCENT_CST=CST_A/TOTAL) #checked calculation by using mutate first
+head(accom.coast.bhi)
+glimpse(accom.coast.bhi)
+print(accom.coast.bhi,n=300)
+
+#check Flag.and.Footnotes - 'b' just stands for break in time series
+accom.coast.bhi = select(accom.coast.bhi, -Flag.and.Footnotes)
+
+#plot - check variation in percent coastal among years for NUTS1_ID regions
+windows(50,30)
+ggplot(accom.coast.bhi )+geom_point(aes(TIME,PERCENT_CST), size=3) +
+  facet_wrap(~NUTS1_ID)+ ylim(0,1)+ xlim(2011.5,2014.5)
+#very consistent - all have 2 or 3 years of data
+
+#get single value across all years
+accom.coast.bhi =accom.coast.bhi %>%
+  group_by(NUTS2_ID,NUTS1_ID)%>%
+  summarise(PERCENT_CST_MEAN = mean(PERCENT_CST, na.rm=TRUE))
+
+accom.coast.bhi  #this can be used to convert NUTS2 data into the fraction that is Coastal
+
+
+
+#################################
+####Apply the average percent coastal to all the
+
+glimpse(bhi.accom.join)
+accom.coast.bhi
 
