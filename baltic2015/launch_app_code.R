b9e4dd2ae92017e0e7d9936b7e2bf40728d91c9f
Julia Stewart Lowndes <jstewart@nceas.ucsb.edu>
2016-02-26 16:38:25 -0800

    added # install.packages('shiny')

--- baltic2015/launch_app_code.R
+++ baltic2015/launch_app_code.R
@@ -1,5 +1,5 @@
 ## to locally run the interactive shiny app, for draft branch only.
 
-library(shiny)
+library(shiny) # install.packages('shiny')
 setwd('~/github/bhi/baltic2015')
 runApp('~/github/bhi/baltic2015/shiny')
