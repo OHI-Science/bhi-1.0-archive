442818dfb63aeacb1d08f2d55452765351463f15
Melsteroni <frazier@nceas.ucsb.edu>
2016-03-18 09:13:30 -0700

    SPP data organize

--- baltic2015/prep/spatial/HELCOM_2_baltic.R
+++ baltic2015/prep/spatial/HELCOM_2_baltic.R
@@ -8,22 +8,27 @@ library(raster)
 library(rgeos)
 library(maptools)
 
-#### BHI map
-bhi <- readOGR(dsn = "../regions", layer = "rgn_offshore_gcs")
+source('../ohiprep/src/R/common.R')
 
+
+#### BHI map
+bhi <- readOGR(dsn = "/var/data/ohi/git-annex/clip-n-ship/bhi/spatial", layer = "rgn_offshore_gcs")
+
+
+### Using the template from
 ### Getting a HELCOM map
-helcom <- readOGR(dsn= 'benthos/Agrypnetes crassicornis (DD)',
+helcom <- readOGR(dsn= '/var/data/ohi/git-annex/Baltic/HELCOM_to_BHIregion/raw/Agrypnetes crassicornis (DD)',
                    layer='Join_Benthic_invertebrates')
 helcom <- helcom[, c("FID_1", "CELLCODE")]
 
 # # testing ignore (this creates a smaller dataset to explore more easily)
 # helcom <- helcom[, c("FID_1", "CELLCODE", "Abra_prism")]
 # helcom@data$Abra_prism <- ifelse(sp_map@data$Abra_prism==0,
-#                                  NA, sp_map@data$Abra_prism) 
+#                                  NA, sp_map@data$Abra_prism)
 # helcom@data$Abra_prism <- factor(helcom@data$Abra_prism)
-# helcom <- helcom[!is.na(helcom@data$Abra_prism), ]  
+# helcom <- helcom[!is.na(helcom@data$Abra_prism), ]
 
-#### transform the sp_map data to have the same 
+#### transform the sp_map data to have the same
 #### coordinate reference system as the region boundaries
 bhi <- spTransform(bhi, CRS(proj4string(helcom)))
 
@@ -34,11 +39,19 @@ plot(helcom)
 #      cex=0.5, font=2, offset=0.5, adj=c(0,2))
 plot(bhi, add=TRUE, col="red")
 
-## this code came from here: 
+## this code came from here:
 ## http://gis.stackexchange.com/questions/140504/extracting-intersection-areas-in-r
-pi <- intersect(helcom, bhi, progress="text")
+pi <- raster::intersect(helcom, bhi)
 areas <- data.frame(area=sapply(pi@polygons, FUN=function(x) {slot(x, 'area')}))
 row.names(areas) <- sapply(pi@polygons, FUN=function(x) {slot(x, 'ID')})
-# Combine attributes info and areas 
+# Combine attributes info and areas
 attArea <- spCbind(pi, areas)
-aggregate(area~rgn_id + CELLCODE, data=attArea, FUN=sum)
+data <- aggregate(area~rgn_id + CELLCODE, data=attArea, FUN=sum)
+tail(arrange(data, CELLCODE), n=100)
+hist(data$area)
+
+# cut cells that have a super small area (only cuts 4 rows)
+data2 <- data[data$area > 1000, ]
+
+write.csv(data2, "baltic2015/prep/spatial/helcom_to_rgn.csv", row.names=FALSE)
+
