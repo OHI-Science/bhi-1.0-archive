576e547eac7c6041460dec906c8d27b6d2caaa54
jules32 <jstewart@nceas.ucsb.edu>
2016-04-01 14:59:35 -0700

    add conditional so don't overwrite README

--- baltic2015/prep/common.r
+++ baltic2015/prep/common.r
@@ -22,18 +22,22 @@ dir_prep   = file.path(dir_baltic, 'prep')
 ## create_readme function will create a rawgit.com url
 create_readme = function(dir = dir_secchi, file = 'secchi_prep.rmd') {
 
-  # create rawgit.com url
-  wd = file.path(dir, file)
-  w  = str_split(wd, '/')
-  fp = file.path('https://cdn.rawgit.com/OHI-Science', w[[1]][3], 'draft', w[[1]][4],
-                 w[[1]][5], w[[1]][6], w[[1]][7],
-                 paste0(file_path_sans_ext(w[[1]][8]), '.html'))
+  # if statement so don't overwrite existing README.md
+  if (!file.exists(file.path(dir, 'README.md'))) {
 
-# rewrite template README.md
-  readLines(file.path(dir_prep, 'README_prep_template.md')) %>%
-    str_replace("here", paste0("[here](", fp, ")")) %>%
-    writeLines(file.path(dir, 'README.md'))
+    # create rawgit.com url
+    wd = file.path(dir, file)
+    w  = str_split(wd, '/')
+    fp = file.path('https://cdn.rawgit.com/OHI-Science', w[[1]][3], 'draft', w[[1]][4],
+                   w[[1]][5], w[[1]][6], w[[1]][7],
+                   paste0(file_path_sans_ext(w[[1]][8]), '.html'))
 
+    # rewrite template README.md
+    readLines(file.path(dir_prep, 'README_prep_template.md')) %>%
+      str_replace("here", paste0("[here](", fp, ")")) %>%
+      writeLines(file.path(dir, 'README.md'))
+
+  }
 }
 
 
