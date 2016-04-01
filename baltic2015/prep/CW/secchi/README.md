c25a2fdb8871f8b47296f06e6365294084a823bd
jules32 <jstewart@nceas.ucsb.edu>
2016-04-01 14:37:54 -0700

    update rawgit url

--- baltic2015/prep/CW/secchi/README.md
+++ baltic2015/prep/CW/secchi/README.md
@@ -1,1 +1,1 @@
-View the prep `.Rmd` file rendered as .html [here](https://rawgit.com/OHI-Science/github/draft/bhi/baltic2015/prep/CW/secchi/secchi_prep.html).
+View the prep `.Rmd` file rendered as .html [here](https://cdn.rawgit.com/OHI-Science/github/draft/bhi/baltic2015/prep/CW/secchi/secchi_prep.html).
--- baltic2015/prep/common.r
+++ baltic2015/prep/common.r
@@ -25,7 +25,7 @@ create_readme = function(dir = dir_secchi, file = 'secchi_prep.rmd') {
   # create rawgit.com url
   wd = file.path(dir, file)
   w  = str_split(wd, '/')
-  fp = file.path('https://rawgit.com/OHI-Science', w[[1]][2], 'draft', w[[1]][3], w[[1]][4],
+  fp = file.path('https://cdn.rawgit.com/OHI-Science', w[[1]][2], 'draft', w[[1]][3], w[[1]][4],
                  w[[1]][5], w[[1]][6], w[[1]][7],
                  paste0(file_path_sans_ext(w[[1]][8]), '.html'))
 
