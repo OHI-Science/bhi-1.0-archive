ab0f68788be5c9c4c96ec3b883f78bde67f127c7
jules32 <jstewart@nceas.ucsb.edu>
2016-04-01 14:22:41 -0700

    added create_readme() function in common.r

--- baltic2015/prep/CW/secchi/README.md
+++ baltic2015/prep/CW/secchi/README.md
@@ -1,1 +1,1 @@
-View secchi_prep.Rmd rendered as an .html file [here](https://rawgit.com/OHI-Science/bhi/draft/baltic2015/prep/CW/secchi/secchi_prep.html).
+View the prep `.Rmd` file rendered as .html [here](https://rawgit.com/OHI-Science/github/draft/bhi/baltic2015/prep/CW/secchi/secchi_prep.html).
--- baltic2015/prep/common.r
+++ baltic2015/prep/common.r
@@ -11,9 +11,29 @@ library(tidyr)
 library(ggplot2)
 library(RMySQL)
 library(stringr)
+library(tools)
 
 ## Directories
 dir_baltic = '~/github/bhi/baltic2015'
 dir_layers = file.path(dir_baltic, 'layers')
 dir_prep   = file.path(dir_baltic, 'prep')
+
+
+## create_readme function will create a rawgit.com url
+create_readme = function(dir = dir_secchi, file = 'secchi_prep.rmd') {
+
+  # create rawgit.com url
+  wd = file.path(dir, file)
+  w  = str_split(wd, '/')
+  fp = file.path('https://rawgit.com/OHI-Science', w[[1]][2], 'draft', w[[1]][3], w[[1]][4],
+                 w[[1]][5], w[[1]][6], w[[1]][7],
+                 paste0(file_path_sans_ext(w[[1]][8]), '.html'))
+
+# rewrite template README.md
+  readLines(file.path(dir_prep, 'README_prep_template.md')) %>%
+    str_replace("here", paste0("[here](", fp, ")")) %>%
+    writeLines(file.path(dir, 'README.md'))
+
+}
+
 
