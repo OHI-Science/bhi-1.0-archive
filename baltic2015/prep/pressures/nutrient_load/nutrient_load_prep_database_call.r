35756e78b21c35ddf36116b7d4bd0904cbe69e4e
jennifergriffiths <jennifer.griffiths@su.se>
2016-04-19 14:10:55 +0200

    nutrient load pressure prep begun

---
+++ baltic2015/prep/pressures/nutrient_load/nutrient_load_prep_database_call.r
@@ -0,0 +1,9 @@
+## nutrient_load_prep_database_call.r
+
+## This file calls the data from the database and saves as a cvs in nutrient_data_database
+
+
+## TO DO ##
+
+## Update this file to extract data from the database when Marc has uploaded.
+## Right now, data were copied directly into the nutrient_data_database folder
