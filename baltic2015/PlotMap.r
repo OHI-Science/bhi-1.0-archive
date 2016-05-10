ab3b0e84c0a982056cad2836a88a59ce9cd41ae9
Casey O'Hara <ohara@nceas.ucsb.edu>
2016-05-09 17:10:27 -0700

    works with ggplot... next up: tmap!

--- baltic2015/PlotMap.r
+++ baltic2015/PlotMap.r
@@ -7,54 +7,72 @@
 library(ggplot2) # install.packages('ggplot2')
 library(RColorBrewer) # install.packages('RColorBrewer')
 
-PlotMap <- function(scores          = scores, # dataframe with at least 2 columns: rgn_id and scores/values.
-                    fld_value_id    = 'region_id', # likely 'rgn_id' or 'region_id' of map regions
-                    fld_value_score = 'score', # likely 'score' or 'value' to display on map
-                    spatial_regions = spatial_regions,
-                    path_figures    = 'reports/figures',
-                    fig_png   = sprintf('%s/map_%s.png', path_figures, g),
-                    # allow fig_png to be NULL and then pass it back as a list of ggplot objects so that you could modify it more on
-                    scale_label     = element_blank(),
+library(dplyr)
+library(tidyr)
+setwd('baltic2015')
+scores_all <- read.csv('scores.csv')
+scores <- scores_all %>%
+  filter(goal == 'BD' & dimension == 'score') %>%
+  filter(region_id != 0)
+
+fld_rgn <- 'region_id'
+fld_score <- 'score'
+scale_limits <- c(0, 100)
+map_title       = 'craptastic'
+scale_label     = 'Biodiversity'
+
+
+source('PrepSpatial.R')
+sp_rgn          = PrepSpatial('spatial/regions_gcs.geojson')
+PlotMap(scores, sp_rgn = sp_rgn, scale_label = 'test1', map_title = 'test2')
+
+PlotMap <- function(scores,         # dataframe with at least 2 columns: rgn_id and scores/values.
+                    sp_rgn          = PrepSpatial('spatial/regions_gcs.geojson'), ### default for OHI+
+                    map_title       = element_blank(),
+                    scale_label     = 'score',
                     scale_limits    = c(0, 100),
+                    print_map       = TRUE, ### print to display
+                    fig_path        = NULL, ### path to save the plot as an image
+                    # allow fig_png to be NULL and then pass it back as a list of ggplot objects so that you could modify it more on
                     overwrite       = TRUE) {
 
+  ### rename columns for convenience...
+  names(scores)[names(scores) == fld_rgn]   <- 'rgn_id'
+  names(sp_rgn)[names(sp_rgn) == fld_rgn]   <- 'rgn_id'
+  names(scores)[names(scores) == fld_score] <- 'score'
 
-    map_title = sprintf('Ocean Health Index scores: %s', g)
-
-      ## join polygon with scores
-    sp_regions <- spatial_regions %>%
-      left_join(scores_g, by = fld_value_id)
-
-
-    ## plot map!
-    res = 100
-    if (overwrite | !file.exists(fig_png)){
-      png(fig_png, width=res*7, height=res*7)
-
-      df_plot = ggplot(data = sp_regions,
-                       aes(x = long, y = lat, group = group, fill = score)) +
-        theme(axis.ticks = element_blank(), axis.text = element_blank(),
-              text = element_text(family = 'Helvetica', color = 'gray30', size = 12),
-              plot.title = element_text(size = rel(1.5), hjust = 0, face = 'bold'),
-              legend.position = 'right') +
-        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank()) +
-        scale_fill_gradientn(colours = brewer.pal(10, 'RdYlBu'), na.value = 'gray80',
-                             limits = scale_limits) +
-        geom_polygon(color = 'gray80', size = 0.1) +
-        labs(title = map_title,
-             fill  = element_blank(),
-             x = NULL, y = NULL)
-
-      ## if save plots as .pngs
-
-      print(df_plot) # try ggsave(fig_png, df_plot) someday, but was taking forever
-      dev.off()
-
-      ## else
-      ## return(df_plot) -- save this into a list. create an empty list and add to it.
-      # can set the name of the item in the list to be the name of the goal.
-
-    }
+  ### join polygon with scores
+  score_rgn <- sp_rgn %>%
+    left_join(scores %>%
+                dplyr::select(rgn_id, score),
+              by = 'rgn_id')
 
 
+  df_plot <- ggplot(data = score_rgn,
+                    aes(x = long, y = lat, group = group, fill = score)) +
+    theme(axis.ticks = element_blank(),
+          axis.text  = element_blank(),
+          axis.title = element_blank(),
+          text       = element_text(family = 'Helvetica', color = 'gray30', size = 12),
+          plot.title = element_text(size = rel(1.5), hjust = 0, face = 'bold'),
+          legend.position = 'right') +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank()) +
+    scale_fill_gradientn(colours = brewer.pal(10, 'RdYlBu'),
+                         na.value = 'gray80',
+                         limits = scale_limits,
+                         name = scale_label) +
+    geom_polygon(color = 'gray80', size = 0.1) +
+    labs(title = map_title)
+
+  if(print_map) {
+    print(df_plot)
+  }
+
+  if(!is.null(fig_path)) {
+    ggsave(fig_path, plot = df_plot, width = 7, height = 7)
+  }
+
+  return(invisible(df_plot))
 }
