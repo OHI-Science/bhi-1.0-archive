b5e4ac84c2b692df447622e1275493be94ca9cad
jules32 <jstewart@nceas.ucsb.edu>
2016-05-09 17:34:46 -0700

    in progress with @oharac...PlotMapMulti.r

--- baltic2015/PlotMapMulti.r
+++ baltic2015/PlotMapMulti.r
@@ -1,28 +1,25 @@
-## PlotMap plots data frame values onto a map with region identifiers, and saves output
-## If there are multiple goals, will loop through and map all
+## PlotMapMulti
+## Loops through multiple goals and map all calling PlotMap
 
+library(dplyr)
 
+# prep_sp = PrepSpatial('spatial/regions_gcs.geojson')
 
+PlotMapMulti <- function(scores          = read.csv('scores.csv'), # dataframe with regions, goals, dimensions, scores
+                         spatial_regions = prep_sp, # PrepSpatial('spatial/regions_gcs.geojson'),
+                         fld_value_id    = 'region_id', # likely 'rgn_id' or 'region_id' of map regions
+                         fld_value_score = 'score', # value to display on map
+                         dim_choice      = 'score', # choice of "future", "pressures", "resilience", "score", "status", "trend"
+                         print_map       = TRUE,
+                         save_map        = TRUE,
+                         path_figures    = 'reports/figures',
+                         map_title       = element_blank(),
+                         scale_label     = 'score',
+                         scale_limits    = c(0, 100),
+                         overwrite       = TRUE) {
 
-library(ggplot2) # install.packages('ggplot2')
-library(RColorBrewer) # install.packages('RColorBrewer')
-
-PlotMap <- function(scores          = scores, # dataframe with at least 2 columns: rgn_id and scores/values.
-                    fld_value_id    = 'region_id', # likely 'rgn_id' or 'region_id' of map regions
-                    fld_value_score = 'score', # likely 'score' or 'value' to display on map
-                    dim_choice      = 'score', # choice of "future", "pressures", "resilience", "score", "status", "trend"
-                    spatial_regions = spatial_regions,
-                    path_figures    = 'reports/figures',
-                    # allow fig_png to be NULL and then pass it back as a list of ggplot objects so that you could modify it more on
-                    scale_label     = element_blank(),
-                    scale_limits    = c(0, 100),
-                    overwrite       = TRUE) {
-  ## debug ## scores = scores; fld_value_id = 'region_id'; fld_value_score = 'score'; dim_choice = 'score'; spatial_regions = spatial_regions; path_figures = 'reports/figures'; scale_label = element_blank(); scale_limits = c(0, 100); overwrite = TRUE
 
   ## setup ----
-
-  ## if scores variable doesn't exist, read in scores.csv
-  if (!exists("scores")) scores = read.csv('scores.csv')
 
   ## check field values in scores column names
   if ( !fld_value_score %in% names(scores) | !fld_value_id %in% names(scores) ) {
@@ -30,56 +27,43 @@ PlotMap <- function(scores          = scores, # dataframe with at least 2 column
                  fld_value_score, fld_value_id))
   }
 
-  ## see if there are multiple goals
-  if( 'goal' %in% colnames(scores) ) {
-    goals = unique(scores$goal)
-  } else {
-    goals = NULL
+  ## if exists, remove region_id == 0 for mapping
+  if (0 %in% scores[[fld_value_id]]){
+    scores <- scores[scores[[fld_value_id]] != 0, ] # figure this out with filter() someday
+  }
+
+  ## if exists, filter dimension for 'score'
+  if ( 'dimension' %in% names(scores) ) {
+    scores <- scores %>%
+      filter(dimension == dim_choice)
   }
 
   ## loop over each goal and subgoal ----
 
-  for (g in goals){ # g ='AO'
+  goals <- unique(scores$goal)
+  for (g in goals){ # g ='BD'
 
     print(sprintf('Mapping %s . . .', g))
-    fig_png   = sprintf('%s/map_%s.png', path_figures, g)
-    map_title = sprintf('Ocean Health Index scores: %s', g)
 
     ## filter scores for goal g
-    scores_g = scores %>%
+    scores_g <-  scores %>%
       filter(goal == g)
-
-    ## if exists, filter dimension for 'score'
-    if ( 'dimension' %in% names(scores_g) ) {
-      scores_g = scores_g %>%
-        filter(dimension == dim_choice)
-    }
-
-    ## if exists, remove region_id == 0 for mapping
-    if (0 %in% scores_g[[fld_value_id]]){
-      scores_g = scores_g[scores_g[[fld_value_id]] != 0, ] # figure this out with filter() someday
-    }
-
-    ## join polygon with scores
-    sp_regions <- spatial_regions %>%
-      left_join(scores_g, by = fld_value_id)
 
 
     ## plot map!
-    PlotMap() ## TODO
+    PlotMap(scores_g,
+            sp_rgn       = spatial_regions,
+            # fld_rgn      = fld_value_id,
+            # fld_score    = fld_value_score,
+            print_map    = print_map,
+            fig_path     = sprintf('%s/map_%s.png', path_figures, g),
+            map_title    = map_title,
+            scale_label  = scale_label,
+            scale_limits = scale_limits,
+            overwrite    = overwrite)
 
-
-      ## if save plots as .pngs
-
-      print(df_plot) # try ggsave(fig_png, df_plot) someday, but was taking forever
-      dev.off()
-
-      ## else
-      ## return(df_plot) -- save this into a list. create an empty list and add to it.
-      # can set the name of the item in the list to be the name of the goal.
-
-    }
 
   }
+
 
 }
