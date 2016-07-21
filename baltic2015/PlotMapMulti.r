75ff6f8267efefb22e9994d4a9f8437cafa6c450
jules32 <jstewart@nceas.ucsb.edu>
2016-07-21 12:10:14 +0100

    testing with plotly--causes RStudio to crash at the moment

--- baltic2015/PlotMapMulti.r
+++ baltic2015/PlotMapMulti.r
@@ -8,12 +8,12 @@ PlotMapMulti <- function(scores          = read.csv('scores.csv'), # dataframe w
                          dim_choice      = 'score', # header of scores variable; likely "future", "pressures", "resilience", "score", "status", "trend"
                          print_fig       = FALSE,
                          save_fig        = TRUE,
+                         # active_plotly   = FALSE, ## takes too long, causes RStudio to crash
                          path_figures    = 'reports/figures',
                          scale_label     = 'score',
-                         scale_limits    = c(0, 100),
-                         overwrite       = TRUE) {
+                         scale_limits    = c(0, 100)) {
                          # TODO: interactive = FALSE
-  # DEBUG: scores=read.csv('scores.csv'); spatial_poly = readOGR(dsn = normalizePath(mapfile_path), "OGRGeoJSON"); fld_value_id = 'region_id'; fld_value_score = 'score';dim_choice = 'score'; print_fig = TRUE; save_fig = TRUE; path_figures = 'reports/figures'; map_title= element_blank();  scale_label = 'score';  scale_limits = c(0, 100);overwrite = TRUE
+  # DEBUG: scores=read.csv('scores.csv'); spatial_poly = readOGR(dsn = normalizePath(mapfile_path), "OGRGeoJSON"); fld_value_id = 'region_id'; fld_value_score = 'score';dim_choice = 'score'; print_fig = TRUE; save_fig = TRUE; path_figures = 'reports/figures'; map_title= element_blank();  scale_label = 'score';  scale_limits = c(0, 100)
 
   ## setup ----
 
@@ -55,9 +55,8 @@ PlotMapMulti <- function(scores          = read.csv('scores.csv'), # dataframe w
             fig_path     = sprintf('%s/map_%s.png', path_figures, g),
             map_title    = sprintf('Ocean Health Index: %s', g),
             scale_label  = scale_label,
-            scale_limits = scale_limits,
-            overwrite    = overwrite)
-    #DEBUG scores = scores_g; rgn_poly = spatial_poly; fld_rgn= fld_value_id;fld_score = fld_value_score;print_fig = print_fig; fig_path = sprintf('%s/map_%s.png', path_figures, g); map_title = map_title;scale_label = scale_label;scale_limits = scale_limits; overwrite = overwrite
+            scale_limits = scale_limits)
+    #DEBUG scores = scores_g; rgn_poly = spatial_poly; fld_rgn= fld_value_id;fld_score = fld_value_score;print_fig = print_fig; fig_path = sprintf('%s/map_%s.png', path_figures, g); map_title = map_title;scale_label = scale_label;scale_limits = scale_limits
 
   }
 
