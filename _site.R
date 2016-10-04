ed4451601b7cf4dd193e615645647084709d30a2
bbest <bdbest@gmail.com>
2016-10-04 08:54:57 -0700

    updating app with ohihrepos commit 7c52c23

---
+++ _site.R
@@ -0,0 +1,5 @@
+title           = "Baltic Health Index"
+app_url         = "http://ohi-science.nceas.ucsb.edu/bhi"
+ohirepos_commit = "7c52c23b214bbbdf9505c39381557d0a3b5d5203"
+dir_scenario    = "bhi_draft/baltic2015"
+
