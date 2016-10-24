05f5f31c92b6fb699a472f6f9ce21f1c7904db11
bbest <bdbest@gmail.com>
2016-10-24 15:26:01 -0700

    updating website with ohirepos commit 91f077d

--- _site.R
+++ _site.R
@@ -18,7 +18,7 @@ gh_repo         = "bhi"
 gh_branch_data  = "draft"
 scenario_dir    = "baltic2015"
 app_url         = "http://ohi-science.nceas.ucsb.edu/bhi"
-ohirepos_commit = "2f23cd258996ab4cb9169e88512229238463cfb8"
+ohirepos_commit = "91f077d8e3f5fc08e21b04eab26c87d2050b6e9e"
 map_shrink_pct  = 5
 
 # derived vars
