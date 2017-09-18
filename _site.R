8acbc3921499584c616151e65cbe805d3f7021d0
jules32 <jstewart@nceas.ucsb.edu>
2017-09-18 11:59:41 -0700

    rework _site.R to update draft's goals/*.Rmd

--- _site.R
+++ _site.R
@@ -1,53 +1,68 @@
+## load any libraries needed across website pages
 suppressPackageStartupMessages({
   library(readr)
   library(dplyr)
   library(tidyr)
-  library(sp)
-  library(geojsonio)
-  library(leaflet)
-  library(htmltools)
-  library(DT)
+  library(stringr)
   library(knitr)
-  library(printr)  # devtools::install_github('yihui/printr')
-  library(ohicore) # devtools::install_github('ohi-science/ohicore')
 })
 
-# brewed vars
-study_area      = "Baltic"
-gh_repo         = "bhi"
-gh_branch_data  = "draft"
-scenario_dir    = "baltic2015"
-app_url         = "http://ohi-science.nceas.ucsb.edu/bhi"
-ohirepos_commit = "af3aaaf9728f4d20d054d420376bde7afd2106c9"
-map_shrink_pct  = 5
+## brewed vars
+study_area      <- "Baltic"
+key             <- "bhi"
+dir_scenario_gh <- "https://raw.githubusercontent.com/OHI-Science/bhi/draft/baltic2015"
 
-# derived vars
-dir_data        = sprintf('%s_%s', gh_repo, gh_branch_data)
-dir_scenario    = sprintf('%s/%s', dir_data, scenario_dir)
-gh_url          = sprintf('https://github.com/OHI-Science/%s.git', gh_repo)
+## knitr options for all webpages
+knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
 
-# knitr options
-knitr::opts_chunk$set(echo = F, message = F, warning = F)
+## read in variables if they exist (i.e. don't try for prep repos)
+scores_csv <- file.path(dir_scenario_gh, 'scores.csv')
+layers_csv <- file.path(dir_scenario_gh, 'layers.csv')
+conf_csv   <- file.path(dir_scenario_gh, 'conf/goals.csv')
 
-run_cmd = function(cmd){
- # cat(sprintf('running command:\n  %s\n', cmd)) #comment out so not displayed on web pages
-  system.time(system(cmd))
-}
+if (RCurl::url.exists(scores_csv)) scores <- readr::read_csv(scores_csv)
+if (RCurl::url.exists(layers_csv)) layers <- readr::read_csv(layers_csv)
+if (RCurl::url.exists(conf_csv))   weight <- readr::read_csv(conf_csv) %>%
+  select(goal, weight)
 
-# data branch: fetch existing, or clone new
-if (!file.exists(dir_data)){
-
-  # clone data branch, shallowly and quietly
-  run_cmd(sprintf('git clone -q --depth 1 --branch %s %s %s', gh_branch_data, gh_url, dir_data))
-} else {
-
-  # git fetch & overwrite
-  run_cmd(sprintf('cd %s; git fetch -q; git reset -q --hard origin/%s; git checkout -q %s; git pull -q', dir_data, gh_branch_data, gh_branch_data))
+## if variables don't exist give a message
+if ( !all( (!RCurl::url.exists(scores_csv)) | (!RCurl::url.exists(layers_csv)) | (!RCurl::url.exists(conf_csv))) ) {
+ message('scores.csv, layers.csv, or conf/goals.Rmd may be missing')
 }
 
 
-# read config
-config = new.env()
-source(file.path(dir_scenario, 'conf/config.R'), config)
+## save local copy of conf/goals.Rmd
+conf_goals_rmd <- file.path(dir_scenario_gh, 'conf/goals.Rmd')
+
+if (RCurl::url.exists(scores_csv)) {
+  conf_goals <- readr::read_lines(conf_goals_rmd)
+  readr::write_lines(conf_goals, path = 'conf_goals.Rmd', append = FALSE)
+}
+
+
+## save local copies of conf/goals/*.Rmd
+dir_conf_goals_web   <- file.path(dir_scenario_gh, 'conf/goals')
+dir_conf_goals_local <- file.path('goals')
+
+if (!exists(dir_conf_goals_local)) dir.create(dir_conf_goals_local)
+
+for (i in weight$goal) { # i = 'FP'
+
+  ## account for upper or lowercase
+  fn_upper <- sprintf('%s/%s.Rmd', dir_conf_goals_web, i)
+  fn_lower <- sprintf('%s/%s.Rmd', dir_conf_goals_web, stringr::str_to_lower(i))
+
+  message(sprintf('trying %s...\n', i))
+
+  if (RCurl::url.exists(fn_upper)) {
+    readr::read_lines(fn_upper) %>%
+      readr::write_lines(path = sprintf('%s/%s.Rmd', dir_conf_goals_local, i), append = FALSE)
+
+  } else if (RCurl::url.exists(fn_lower)) {
+    readr::read_lines(fn_lower) %>%
+      readr::write_lines(path = sprintf('%s/%s.Rmd', dir_conf_goals_local, i), append = FALSE)
+  }
+}
+
 
 
