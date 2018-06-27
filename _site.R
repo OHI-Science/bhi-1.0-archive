1bbe508d8f3c9f67cc06bc00094163a4f2384e59
jules32 <jstewart@nceas.ucsb.edu>
2018-06-27 13:34:58 -0700

    modernizing _site.R; use rawgit rather than saving so many local files

--- _site.R
+++ _site.R
@@ -1,8 +1,6 @@
 ## load any libraries needed across website pages
 suppressPackageStartupMessages({
-  library(readr)
-  library(dplyr)
-  library(tidyr)
+  library(tidyverse)
   library(stringr)
   library(knitr)
 })
@@ -11,63 +9,45 @@ suppressPackageStartupMessages({
 study_area      <- "Baltic"
 key             <- "bhi"
 dir_scenario_gh <- "https://raw.githubusercontent.com/OHI-Science/bhi/draft/baltic2015"
-app_url         <- "http://ohi-science.nceas.ucsb.edu/bhi"
 
 ## knitr options for all webpages
 knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
 
-## read in variables if they exist (i.e. don't try for prep repos)
-scores_csv <- file.path(dir_scenario_gh, 'scores.csv')
-layers_csv <- file.path(dir_scenario_gh, 'layers.csv')
-conf_csv   <- file.path(dir_scenario_gh, 'conf/goals.csv')
-
-if (RCurl::url.exists(scores_csv)) scores <- readr::read_csv(scores_csv)
-if (RCurl::url.exists(layers_csv)) layers <- readr::read_csv(layers_csv)
-if (RCurl::url.exists(conf_csv))   weight <- readr::read_csv(conf_csv) %>%
+## read in variables
+scores <- readr::read_csv(file.path(dir_scenario_gh, 'scores.csv'))
+layers <- readr::read_csv(file.path(dir_scenario_gh, 'layers.csv'))
+weight <- readr::read_csv(file.path(dir_scenario_gh, 'conf/goals.csv')) %>%
   select(goal, weight)
 
-## if variables don't exist give a message
-if ( !all( (!RCurl::url.exists(scores_csv)) | (!RCurl::url.exists(layers_csv)) | (!RCurl::url.exists(conf_csv))) ) {
- message('scores.csv, layers.csv, or conf/goals.Rmd may be missing')
-}
-
-# read config
-config = new.env()
-source(file.path(dir_scenario_gh, 'conf/config.R'), config)
+#
+# # read config
+# config = new.env()
+# source(file.path(dir_scenario_gh, 'conf/config.R'), config)
 
 
-## save local copy of conf/goals.Rmd
-conf_goals_rmd <- file.path(dir_scenario_gh, 'conf/goals.Rmd')
+## save local copies of Rmds to knit-child ----
 
-if (RCurl::url.exists(scores_csv)) {
-  conf_goals <- readr::read_lines(conf_goals_rmd)
-  readr::write_lines(conf_goals, path = 'conf_goals.Rmd', append = FALSE)
-}
+dir_raw_draft <- 'https://raw.githubusercontent.com/OHI-Science/mhi/master'
+
+to_copy <- c('conf/web/goals.Rmd')
 
 
-## save local copies of conf/goals/*.Rmd
-dir_conf_goals_web   <- file.path(dir_scenario_gh, 'conf/goals')
-dir_conf_goals_local <- file.path('goals')
+for (f in to_copy) { # f <-  'region2017/conf/web/goals.Rmd'
 
-if (!exists(dir_conf_goals_local)) dir.create(dir_conf_goals_local)
+  fp <- file.path(dir_raw_draft, f)
 
-for (i in weight$goal) { # i = 'FP'
-
-  ## account for upper or lowercase
-  fn_upper <- sprintf('%s/%s.Rmd', dir_conf_goals_web, i)
-  fn_lower <- sprintf('%s/%s.Rmd', dir_conf_goals_web, stringr::str_to_lower(i))
-
-  message(sprintf('trying %s...\n', i))
-
-  if (RCurl::url.exists(fn_upper)) {
-    readr::read_lines(fn_upper) %>%
-      readr::write_lines(path = sprintf('%s/%s.Rmd', dir_conf_goals_local, i), append = FALSE)
-
-  } else if (RCurl::url.exists(fn_lower)) {
-    readr::read_lines(fn_lower) %>%
-      readr::write_lines(path = sprintf('%s/%s.Rmd', dir_conf_goals_local, i), append = FALSE)
+  ## if the url exists, save a copy.
+  if (RCurl::url.exists(fp)) {
+    f_web   <- readr::read_lines(fp)
+    if ( tools::file_ext(fp) == 'Rmd' ) {
+      f_local <- paste0('local_', basename(fp))
+    } else {
+      f_local <- basename(fp)
+    }
+    readr::write_lines(f_web, path = f_local, append = FALSE)
+    message(sprintf('saving %s', f_local))
+  } else {
+    message(sprintf('%s does not exist', fp))
   }
 }
-
-
 
